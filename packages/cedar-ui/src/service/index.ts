cc4e24c205330425199e816cac20d19b6c9b5298
aminnausin <aminnausin@outlook.com>
2025-08-03 19:16:54 -0400

    chore: remove components from package

--- packages/cedar-ui/src/service/index.ts
+++ packages/cedar-ui/src/service/index.ts
@@ -1,5 +1,4 @@
 export * from '@/service/util';
-export * from '@/service/toaster/component';
 export * from '@/service/toaster/constants';
 export * from '@/service/toaster/toastService';
 export * from '@/service/sort/strategies';
