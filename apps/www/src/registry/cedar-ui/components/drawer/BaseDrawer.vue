49b09c60e31e00b40ce7eadb7537a982d0bbde7d
aminnausin <aminnausin@outlook.com>
2025-12-14 01:32:49 -0500

    fix: max content height and scrollbars on drawer missing

--- apps/www/src/registry/cedar-ui/components/drawer/BaseDrawer.vue
+++ apps/www/src/registry/cedar-ui/components/drawer/BaseDrawer.vue
@@ -31,38 +31,49 @@ const { offset, isSwiping, onPointerDown, onPointerMove, onPointerUp } = useSwip
             '--offset-y': `${Math.max(-0, Math.min(offset.y, SWIPE_LIMIT))}px`,
         }"
     >
-        <div class="flex flex-col p-3 gap-6 rounded-t-xl items-center justify-center">
+        <div class="flex flex-col rounded-t-xl items-center justify-center">
             <div
-                class="w-full cursor-pointer group"
+                class="w-full cursor-pointer group pt-4"
                 :data-isSwiping="isSwiping"
                 @pointerdown="onPointerDown"
                 @pointermove="onPointerMove"
                 @pointerup="onPointerUp"
                 @pointercancel="onPointerUp"
             >
-                <div
-                    class="mx-auto w-1/8 sm:w-1/12 h-1.5 rounded-full bg-foreground-2 cursor-pointer group-hover:bg-foreground-1 transition-colors"
-                ></div>
+                <slot name="handle">
+                    <div
+                        class="mx-auto w-1/8 sm:w-1/12 h-1.5 rounded-full bg-foreground-2 cursor-pointer group-hover:bg-foreground-1 transition-colors"
+                    ></div>
+                </slot>
             </div>
-            <div class="flex flex-col gap-1.5 w-full">
-                <h2 id="drawerTitle" class="flex-1 text-xl font-semibold text-foreground">
-                    <slot name="title">
-                        {{ drawer.props.value.title ?? 'Title' }}
-                    </slot>
-                </h2>
-                <p
-                    class="w-full text-sm text-foreground-1"
-                    v-if="$slots.description || drawer.props.value.description"
-                    id="drawerDescription"
-                >
-                    <slot name="description">{{ drawer.props.value.description ?? 'Description' }}</slot>
-                </p>
+            <div class="p-3 flex flex-col gap-3 w-full">
+                <slot name="header">
+                    <div class="flex flex-col gap-1.5 w-full">
+                        <h2 id="drawerTitle" class="flex-1 text-xl font-semibold text-foreground">
+                            <slot name="title">
+                                {{ drawer.props.value.title ?? 'Title' }}
+                            </slot>
+                        </h2>
+                        <p
+                            class="w-full text-sm text-foreground-1"
+                            v-if="$slots.description || drawer.props.value.description"
+                            id="drawerDescription"
+                        >
+                            <slot name="description">{{ drawer.props.value.description ?? 'Description' }}</slot>
+                        </p>
+                    </div>
+                </slot>
             </div>
-            <slot>
-                <ButtonText :class="'text-sm h-8 ring-1 capitalize w-full'" :variant="'default'" @click="drawer.close()">
-                    Close Drawer
-                </ButtonText>
-            </slot>
+            <div v-if="$slots.default" class="max-h-[60vh] overflow-y-scroll scrollbar-hide flex flex-col gap-3 w-full p-3">
+                <slot> </slot>
+            </div>
+            <div v-if="$slots.footer || !$slots.default" class="p-3 flex flex-col gap-3 w-full">
+                <slot name="footer">
+                    <ButtonText :class="'text-sm h-8 ring-1 capitalize w-full'" :variant="'default'" @click="drawer.close()">
+                        Close Drawer
+                    </ButtonText>
+                </slot>
+            </div>
         </div>
     </RootDrawer>
 </template>
