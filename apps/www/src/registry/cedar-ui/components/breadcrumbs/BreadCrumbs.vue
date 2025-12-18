5337ab4aef6e08b572e6176ba79890dba3043fc1
aminnausin <aminnausin@outlook.com>
2025-12-18 02:03:34 -0500

    feat: convert breadcrumbs

--- apps/www/src/registry/cedar-ui/components/breadcrumbs/BreadCrumbs.vue
+++ apps/www/src/registry/cedar-ui/components/breadcrumbs/BreadCrumbs.vue
@@ -1,27 +1,33 @@
 <script setup lang="ts">
 import type { BreadCrumbItem } from '@aminnausin/cedar-ui';
 
+import { cn } from '@aminnausin/cedar-ui';
+
 withDefaults(defineProps<{ breadCrumbs: BreadCrumbItem[] }>(), { breadCrumbs: () => [] });
 </script>
 <template>
     <nav class="flex justify-between flex-1">
-        <ol
-            class="inline-flex items-center space-x-1 text-neutral-500 dark:text-neutral-300 [&_.active-breadcrumb]:text-neutral-600 dark:[&_.active-breadcrumb]:text-white [&_.active-breadcrumb]:font-medium sm:mb-0"
-        >
+        <ol class="inline-flex items-center space-x-1 text-foreground-2 sm:mb-0">
             <template v-for="(breadCrumb, index) in breadCrumbs" :key="index">
                 <li class="flex items-center h-full">
                     <RouterLink
                         :to="breadCrumb.url"
-                        class="transition-colors inline-flex items-center p-2 py-1.5 space-x-1.5 rounded-md hover:text-neutral-900! dark:hover:text-white! hover:ring-2 ring-inset hover:ring-violet-400 hover:bg-white dark:hover:ring-violet-700 dark:hover:bg-primary-dark-800"
-                        :class="{ 'active-breadcrumb ': index === breadCrumbs.length - 1 }"
+                        :class="
+                            cn(
+                                'aria-[current]:font-medium aria-[current]:text-foreground-1 aria-[current]:dark:text-foreground hover:text-foreground-0',
+                                'transition-colors inline-flex items-center p-2 py-1.5 space-x-1.5 rounded-md',
+                                'ring-inset hover:ring-2 hover:ring-primary hover:bg-surface-2',
+                            )
+                        "
                         :title="breadCrumb.name"
+                        :aria-current="index === breadCrumbs.length - 1"
                     >
-                        <component v-if="breadCrumb.icon" :is="breadCrumb.icon" class="w-3.5 h-3.5 shrink-0" />
+                        <component v-if="breadCrumb.icon" :is="breadCrumb.icon" class="size-3.5 shrink-0" />
                         <span class="capitalize max-w-32 line-clamp-1 break-all">{{ breadCrumb.name }}</span>
                     </RouterLink>
                 </li>
                 <svg
-                    class="w-5 h-5 text-gray-400 shrink-0"
+                    class="size-5 text-foreground-2 shrink-0"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     v-if="index < breadCrumbs.length - 1"
@@ -43,6 +49,5 @@ withDefaults(defineProps<{ breadCrumbs: BreadCrumbItem[] }>(), { breadCrumbs: ()
 <style lang="css" scoped>
 a {
     text-decoration: none;
-    color: inherit;
 }
 </style>
