a5e1458732e9bdecdb306a39111e0cb65251dde6
aminnausin <aminnausin@outlook.com>
2025-12-25 22:14:41 -0500

    fix: useTable data watcher triggers on data recreation (edits)

--- packages/cedar-ui/src/components/table/useTable.ts
+++ packages/cedar-ui/src/components/table/useTable.ts
@@ -1,9 +1,9 @@
-import type { UseTableOptions } from './table.types';
+import type { TableRow, UseTableOptions } from './table.types';
 
 import { computed, ref, watch } from 'vue';
 
-export default function useTable<T>(options: UseTableOptions<T>) {
-    const itemsPerPage = ref(options.itemsPerPage ?? 10);
+export default function useTable<T extends TableRow>(options: UseTableOptions<T>) {
+    const itemsPerPage = computed(() => options.itemsPerPage ?? 10);
     const currentPage = ref(1);
 
     const pageCount = computed(() => Math.ceil(options.data.value.length / itemsPerPage.value));
@@ -22,8 +22,13 @@ export default function useTable<T>(options: UseTableOptions<T>) {
 
     watch(
         () => options.data.value,
-        () => {
-            if (options.resetOnDataChange !== false) resetPage();
+        (next, prev) => {
+            if (options.resetOnDataChange === false) return;
+
+            if (!prev) return resetPage(); // On first load
+            if (!next.length || !prev.length) return resetPage(); // Handles empty datasets ?
+            if (next.length !== prev.length) return resetPage(); // If length changes (search, data source change)
+            if (next[0]?.id !== prev[0]?.id) return resetPage(); // If data changes (first item is different but lengths are the same)
         },
         { immediate: true },
     );
